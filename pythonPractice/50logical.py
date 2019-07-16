36cbbfe695428b68a91e48d5324ef2e7079c18ed
Time1996 <736874054@qq.com>
2019-07-16 22:06:27 +0800

    python日常练习

--- pythonPractice/50logical.py
+++ pythonPractice/50logical.py
@@ -1,6 +1,6 @@
 import re
-text = 'Hi, I am Shirley Hilton. I am his wife.'
-m = re.findall(r'\b[Hh]i', text)
+text = 'site sea sue sweet see see see sseee loses'
+m = re.findall(r'\bs\S*e\b', text)#r表示raw不对字符串进行转义,不用的话\会转义
 if m:
     print(m)
 else:
---
+++ pythonPractice/52random.py
@@ -0,0 +1,4 @@
+import random
+a = random.random() * 100#random随机浮点数
+b = random.randrange(2,20,3)#范围2-20,(2,5,8,11,14,17,20)这些数里随机选
+print(a,b)
